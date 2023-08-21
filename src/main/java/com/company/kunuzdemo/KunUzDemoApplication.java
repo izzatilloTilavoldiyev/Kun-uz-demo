0f4d48b4c2d3c4ee51ec2d22f2c214cc8231d350
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-08-21 15:26:14 +0500

    project committed

---
+++ src/main/java/com/company/kunuzdemo/KunUzDemoApplication.java
@@ -0,0 +1,13 @@
+package com.company.kunuzdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunUzDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunUzDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/kunuzdemo/KunUzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.kunuzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
