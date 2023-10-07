4ef6f10959088bd26e5f1584789c585aea673ddd
Bekzod <bekzodhamidullayev16@gmail.com>
2023-10-07 20:58:30 +0500

    update CategoryController update createController metod, created CategoryService(interface)

--- src/main/java/com/company/kunuzdemo/controller/CategoryController.java
+++ src/main/java/com/company/kunuzdemo/controller/CategoryController.java
@@ -2,9 +2,11 @@ package com.company.kunuzdemo.controller;
 
 import com.company.kunuzdemo.dtos.request.CategoryCreateDTO;
 import com.company.kunuzdemo.dtos.response.CategoryDTO;
-import com.company.kunuzdemo.service.CategoryService;
+import com.company.kunuzdemo.service.CategoryServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -13,14 +15,15 @@ import java.util.UUID;
 @RequestMapping("/api/v1/category")
 @RequiredArgsConstructor
 public class CategoryController {
-    private final CategoryService categoryService;
+    private final CategoryServiceImpl categoryService;
     @PostMapping("/create")
-    public CategoryDTO create(@Valid @RequestBody CategoryCreateDTO dto) {
-        return categoryService.create(dto);
+    public ResponseEntity<CategoryDTO> create(@Valid @RequestBody CategoryCreateDTO dto) {
+        CategoryDTO categoryDTO = categoryService.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryDTO);
     }
 
     @GetMapping("/get-by-id/{categoryId}")
-    public CategoryDTO getById(@PathVariable UUID categoryId) {
-        return categoryService.getById(categoryId);
+    public ResponseEntity<CategoryDTO> getById(@PathVariable UUID categoryId) {
+        return ResponseEntity.ok(categoryService.getById(categoryId));
     }
 }
