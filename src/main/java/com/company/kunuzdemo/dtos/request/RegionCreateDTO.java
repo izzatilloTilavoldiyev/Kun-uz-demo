09e90f8d68e850391e9591e9e553d525a79d3a87
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-10-07 20:25:41 +0500

    regin DTOs created and regin repository added

---
+++ src/main/java/com/company/kunuzdemo/dtos/request/RegionCreateDTO.java
@@ -0,0 +1,21 @@
+package com.company.kunuzdemo.dtos.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegionCreateDTO {
+
+    @NotBlank(message = "Name UZ must not be blank")
+    private String nameUZ;
+
+    @NotBlank(message = "Name RU must not be blank")
+    private String nameRU;
+
+    @NotBlank(message = "Name EN must not be blank")
+    private String nameEN;
+}
--- src/main/java/com/company/kunuzdemo/entity/Region.java
+++ src/main/java/com/company/kunuzdemo/entity/Region.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.*;
 
-@Entity
+@Entity(name = "region")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
