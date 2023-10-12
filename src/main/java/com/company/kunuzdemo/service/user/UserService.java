2b466da36a31ddcf6e260c4fe89f43e6ce62f5eb
Bekzod <bekzodhamidullayev16@gmail.com>
2023-10-12 19:18:36 +0500

    add metod(blocById, unblockById, changeRole)

--- src/main/java/com/company/kunuzdemo/service/user/UserService.java
+++ src/main/java/com/company/kunuzdemo/service/user/UserService.java
@@ -3,7 +3,9 @@ package com.company.kunuzdemo.service.user;
 import com.company.kunuzdemo.dtos.response.UserResponseDTO;
 import com.company.kunuzdemo.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
@@ -11,7 +13,12 @@ public interface UserService {
 
     User getUserByEmail(String email);
 
-    Page<UserResponseDTO> getAll(int page, int size);
+    List<UserResponseDTO> getAll(int page, int size);
 
-    Page<UserResponseDTO> filterByRole(int page, int size, String role);
+    List<UserResponseDTO> filterByRole(int page, int size, String role);
+
+    String blocById(UUID userId);
+    String unblockById(UUID userId);
+
+    UserResponseDTO changeRole(UUID userId, String role);
 }
