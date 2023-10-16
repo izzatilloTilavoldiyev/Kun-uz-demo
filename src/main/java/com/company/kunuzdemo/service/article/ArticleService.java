82f7a5f1e713c98bcda5b7ee43fe8948349a5653
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-10-16 19:38:21 +0500

    search by title and change status APIs added

--- src/main/java/com/company/kunuzdemo/service/article/ArticleService.java
+++ src/main/java/com/company/kunuzdemo/service/article/ArticleService.java
@@ -3,6 +3,7 @@ package com.company.kunuzdemo.service.article;
 import com.company.kunuzdemo.dtos.request.ArticleCreateDTO;
 import com.company.kunuzdemo.dtos.response.ArticleResponseDTO;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ArticleService {
@@ -10,4 +11,8 @@ public interface ArticleService {
     ArticleResponseDTO create(ArticleCreateDTO articleCreateDTO);
 
     ArticleResponseDTO getByID(UUID articleID);
+
+    List<ArticleResponseDTO> searchByTitle(String title, Integer page, Integer size);
+
+    String changeStatus(UUID articleID, String status);
 }
