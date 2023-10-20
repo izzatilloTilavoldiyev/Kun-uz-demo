cc2a118824fcde34c289ba85481e1450113a7730
Bekzod <bekzodhamidullayev16@gmail.com>
2023-10-20 10:26:17 +0500

    add role

--- src/main/java/com/company/kunuzdemo/controller/UserController.java
+++ src/main/java/com/company/kunuzdemo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.company.kunuzdemo.controller;
 
 import com.company.kunuzdemo.dtos.request.ChangeRoleDTO;
-import com.company.kunuzdemo.dtos.request.PasswordUpdateDTO;
 import com.company.kunuzdemo.dtos.request.UserUpdateProfileDTO;
 import com.company.kunuzdemo.dtos.response.UserResponseDTO;
 import com.company.kunuzdemo.service.user.UserService;
@@ -9,6 +8,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +21,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getById(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.getById(id));
@@ -35,6 +36,8 @@ public class UserController {
         return ResponseEntity.ok(userService.searchByEmail(email, page, size));
     }
 
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> getAll(
             @RequestParam(required = false, defaultValue = "0") Integer page,
@@ -43,6 +46,8 @@ public class UserController {
         return ResponseEntity.ok(userService.getAll(page, size));
     }
 
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     @GetMapping("/filter-by-role")
     public ResponseEntity<List<UserResponseDTO>> filterByRole(
             @RequestParam(required = false, defaultValue = "0") Integer page,
@@ -52,6 +57,8 @@ public class UserController {
         return ResponseEntity.ok(userService.filterByRole(page, size, role));
     }
 
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @GetMapping("/all-deleted")
     public ResponseEntity<List<UserResponseDTO>> getAllDeleted(
             @RequestParam(required = false, defaultValue = "0") Integer page,
@@ -60,16 +67,22 @@ public class UserController {
         return ResponseEntity.ok(userService.getAllDeleted(page, size));
     }
 
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @PutMapping("/block/{userID}")
     public ResponseEntity<String> blocByID(@PathVariable UUID userID) {
         return ResponseEntity.ok(userService.blockByID(userID));
     }
 
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @PutMapping("/unblock/{userID}")
     public ResponseEntity<String> unblockByID(@PathVariable UUID userID) {
         return ResponseEntity.ok(userService.unblockByID(userID));
     }
 
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @PutMapping("/change-role")
     public ResponseEntity<UserResponseDTO> changeRole(
             @RequestBody @Valid ChangeRoleDTO role
@@ -77,6 +90,8 @@ public class UserController {
         return ResponseEntity.ok(userService.changeRole(role));
     }
 
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @PutMapping("/update-profile/{userID}")
     public ResponseEntity<UserResponseDTO> updateProfile(
             @PathVariable UUID userID,
@@ -86,10 +101,14 @@ public class UserController {
     }
 
 
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     @DeleteMapping("/delete/{userID}")
     ResponseEntity<String> deleteByID(@PathVariable UUID userID) {
         return ResponseEntity.ok(userService.deleteByID(userID));
     }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
 
     @DeleteMapping("/delete-selected")
     public ResponseEntity<String> deleteSelected(@Valid @RequestParam List<UUID> userIDs) {
