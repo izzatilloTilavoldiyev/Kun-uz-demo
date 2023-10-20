c4a159643bbde7a9be079c515f012692ac61ec0d
Bekzod <bekzodhamidullayev16@gmail.com>
2023-10-20 19:12:38 +0500

    add roles and update comment function

--- src/main/java/com/company/kunuzdemo/repository/CommentRepository.java
+++ src/main/java/com/company/kunuzdemo/repository/CommentRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
-    //todo articleID fix
-    @Query(value = "from comment c where c.article.id =: articleId and c.article.deleted = false")
+    @Query(value = "from comment c where c.article.id =:articleId and not c.article.deleted")
     List<Comment> findCommentByArticle(@Param("articleId") UUID articleId);
 }
